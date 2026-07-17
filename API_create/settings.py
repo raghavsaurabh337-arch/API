f97cc2dfaca9d17609813b78ccff2818301cd676
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-18 03:58:39 +0530

    mySQL conncentc

--- API_create/settings.py
+++ API_create/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'API_create.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': 'Raghav@@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
