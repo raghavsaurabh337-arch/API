88df06ca3aaea9c5e1b269cddb5b958823d43b8c
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-09-07 20:04:44 +0530

    bug fix

--- API_create/settings.py
+++ API_create/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'API_create.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'api',
-        'USER': 'root',
-        'PASSWORD': 'Raghav@@123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
